b5e8b82352907ec36a9962fd94f08bcc3f3b6133
Rashidkurban <98862848+caravanguide@users.noreply.github.com>
2022-03-28 18:48:23 -0400

    add first class

--- src/day0/FirstClass.java
+++ src/day0/FirstClass.java
@@ -4,7 +4,11 @@ public class FirstClass {
     public static void main(String[] args) {
         // TODO: WORK ON SOME JAVA TASK
         // extra lie 1
-        
+
+
+
+        //gjhghjgjkjgo
+
 
 
     }
